5e3183e4a6362b4e5942ea9bd487cdd447c9f921
Cmer4TM <cmer4.tm@gmail.com>
2024-05-07 23:51:02 +0100

    Написал окно авторизации на WPF с запросами REGISTER и SIGN IN

--- Sea Battle/CurrentUser.cs
+++ Sea Battle/CurrentUser.cs
@@ -8,10 +8,8 @@ namespace Sea_Battle
 {
     static class CurrentUser
     {
-        public static Form? form = new AuthPage();
-
-        public static string? address;
-        public static int? port;
+        public static string? address = "127.0.0.1";
+        public static int? port = 9001;
 
         public static User? user;
         public static Game? game;
@@ -51,57 +49,6 @@ namespace Sea_Battle
             catch (Exception ex)
             {
                 throw new Exception(ex.Message);
-            }
-        }
-
-        public static void ButtonEnabledChanged(object? sender, EventArgs e)
-        {
-            if (sender is Button button)
-            {
-                button.ForeColor = button.Enabled ? Color.White : Color.Gray;
-                button.BackColor = button.Enabled ? Color.Black : Color.DimGray;
-            }
-        }
-
-        // Пример использования метода SendMessageAsync
-        private static async void Test()
-        {
-            // Создаем обработчик ошибок
-            // Так как метод SendMessageAsync вызывает throw если с сервера была отправлена ошибка
-            // В throw передается свойство ErrorMessage класса Response
-            try
-            {
-                // Создаем запрос на сервер
-                Request request = new()
-                {
-                    // Ключевое слово для сервера
-                    Header = "TEST",
-
-                    // Пользователь который обратился к серверу.
-                    // После входа в аккаунт он сохранен в переменной user класса CurrentUser
-                    User = CurrentUser.user,
-
-                    // Добавляйте нужные параметры в класс Request в зависимости от запроса
-                    // Ожидаемые параметры от клиента я прописал в Trello
-                };
-
-                // Отправка запроса на сервер через метод SendMessageAsync
-                // Адресс и порт сервера сохранены в переменных address и port класса CurrentUser
-                // SendMessageAsync возвращает ответ от сервера Response
-                // Если вам надо использовать ответ сервера, то сохраняйте ответ в переменную
-                Response response = await CurrentUser.SendMessageAsync(request);
-
-                // Делать проверку на ошибку с сервера в свойстве ErrorMessage не нужно
-                // Так как при наличии ошибки будет вызван throw, который перехватит наш catch
-
-                // Если код дошел дальше вызова метода SendMessageAsync, значит ошибки у сервера не было
-                // Это означает что все прошло ОК, и можно делать нужные действия
-
-            }
-            catch (Exception ex)
-            {
-                // Вывод ошибки 
-                MessageBox.Show(ex.Message);
             }
         }
     }
---
+++ Sea Battle/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Sea_Battle.Infrastructure;
+using Models;
+using PropertyChanged;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Sea_Battle.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    class MainWindowViewModel
+    {
+        public string Login { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Address { get; set; } = CurrentUser.address ?? string.Empty;
+        public string Port { get; set; } = CurrentUser.port.ToString() ?? string.Empty;
+        public ICommand EntryCommand { get; }
+        public ICommand ConnectCommand { get; }
+        public MainWindowViewModel()
+        {
+            EntryCommand = new RelayCommand<string>(Entry, CanEntry);
+            ConnectCommand = new RelayCommand(Connect, CanConnect);
+        }
+        public async void Entry(string header)
+        {
+            try
+            {
+                var request = new Request()
+                {
+                    Header = header,
+                    User = new User()
+                    {
+                        Login = Login,
+                        Password = Password
+                    }
+                };
+
+                Response response = await CurrentUser.SendMessageAsync(request);
+
+                CurrentUser.user = response.User;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+                Login = string.Empty;
+                Password = string.Empty;
+            }
+        }
+        public void Connect()
+        {
+            try
+            {
+                CurrentUser.address = Address;
+                CurrentUser.port = int.Parse(Port);
+            }
+            catch
+            {
+                MessageBox.Show("IP Address Field or Port Field are empty!", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        public bool CanConnect()
+            => CurrentUser.address != Address
+            || CurrentUser.port.ToString() != Port;
+        public bool CanEntry()
+            => string.IsNullOrEmpty(Login) == false
+            && string.IsNullOrEmpty(Password) == false;
+    }
+}
